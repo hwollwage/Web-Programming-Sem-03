aeb2df676d31a7f7de722df4f1cae9c0cb9ff7e4
hwollwage <hanzelputrawollwage@gmail.com>
2025-11-05 15:51:18 +0700

    add jobsheet 11 about structural dynamic web with php, sungguh malas sekali

---
+++ Jobsheet-11-PHP_Structural_Dynamic_Web/admin/template/footer.php
@@ -0,0 +1,7 @@
+<script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eITPs3JtLXLISHb3dIISYNnJoS5krbKrcLTnz2xt+EMPSM0VD8xDLt5GGz6uLp" crossorigin="anonymous"></script>
+
+<script src="assets/custom/dashboard.js"></script>
+</body>
+</html>
---
+++ Jobsheet-11-PHP_Structural_Dynamic_Web/cek_login.php
@@ -0,0 +1,35 @@
+<?php
+if(session_status())
+    session_start();
+
+include "config/koneksi.php";
+include "fungsi/pesan_kilat.php";
+include "fungsi/anti_injection.php";
+
+$username = antiinjection($koneksi, $_POST['username']);
+$password = antiinjection($koneksi, $_POST['password']);
+
+$query = "SELECT username, level, salt, password as hashed_password 
+            FROM user WHERE username = '$username'";
+$result = mysqli_query($koneksi, $query);
+$row = mysqli_fetch_assoc($result);
+mysqli_close($koneksi);
+$salt = $row['salt'];
+$hashed_password = $row['hashed_password'];
+
+if($salt !== null & $hashed_password !== null) {
+    $combined_password = $salt . $password;
+
+    if(password_verify($combined_password, $hashed_password)) {
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['level'] = $row['level'];
+        header("Location: index.php");
+    }else {
+        pesan('danger', "login gagal. Password anda salah ");
+        header("Location: login.php");
+    }
+}else {
+    pesan('warning', "username tidak ditemukan");
+    header("Location: login.php");
+}
+?>
